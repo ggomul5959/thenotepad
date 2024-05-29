46b84df7c3968b9cc4485ce7c058cbfbfde99178
ggomul5959 <135287688+ggomul5959@users.noreply.github.com>
2024-05-29 18:17:36 +0900

    Update script.js

--- Currency/script.js
+++ Currency/script.js
@@ -1,10 +1,8 @@
-async function fetchExchangeRates() {
-    const API_KEY = 'lKtQiWfhqrvEn6LD3CL5p7d3fDKb0KOV'; // 여기에 발급받은 API 키를 넣어주세요
-
+asyasync function fetchExchangeRates() {
     try {
-        const response = await fetch(`https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=AUTHKEYlKtQiWfhqrvEn6LD3CL5p7d3fDKb0KOV&searchdate=20180102&data=AP01`);
+        const response = await fetch('https://api.exchangeratesapi.io/latest');
         const data = await response.json();
-        return data;
+        return data.rates;
     } catch (error) {
         console.error('Error fetching exchange rates:', error);
         return {};
@@ -12,8 +10,7 @@ async function fetchExchangeRates() {
 }
 
 async function fetchCurrencies() {
-    const exchangeRates = await fetchExchangeRates();
-    const currencies = exchangeRates.map(rate => rate.cur_unit); // 환율 정보에서 통화 코드 추출
+    const currencies = ['KRW', 'JPY', 'CNY', 'USD', 'EUR']; // 변환할 통화 목록
 
     const selectFrom = document.getElementById('fromCurrency');
     const selectTo = document.getElementById('toCurrency');
@@ -36,13 +33,18 @@ async function convert() {
     const toCurrency = document.getElementById('toCurrency').value;
 
     const exchangeRates = await fetchExchangeRates();
-    const fromRate = exchangeRates.find(rate => rate.cur_unit === fromCurrency).deal_bas_r;
-    const toRate = exchangeRates.find(rate => rate.cur_unit === toCurrency).deal_bas_r;
 
-    const exchangeRate = toRate / fromRate;
-    const result = amount * exchangeRate;
+    const fromRate = exchangeRates[fromCurrency];
+    const toRate = exchangeRates[toCurrency];
 
-    document.getElementById('result').innerText = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
+    if (fromRate && toRate) {
+        const exchangeRate = toRate / fromRate;
+        const result = amount * exchangeRate;
+
+        document.getElementById('result').innerText = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
+    } else {
+        console.error('Invalid currency.');
+    }
 }
 
 document.getElementById('convertButton').addEventListener('click', convert);
