65fae51125f45253b0936c487ea9e6872f768207
ggomul5959 <135287688+ggomul5959@users.noreply.github.com>
2024-06-08 14:42:32 +0900

    Update script.js

--- interest/script.js
+++ interest/script.js
@@ -20,5 +20,5 @@ function calculateInterest() {
     }
 
     document.getElementById('result').innerHTML = 
-        `Interest: ${interest.toFixed(2)}<br>Total Amount: ${totalAmount.toFixed(2)}`;
+        `이자액: ${interest.toFixed(2)}<br>총액: ${totalAmount.toFixed(2)}`;
 }
